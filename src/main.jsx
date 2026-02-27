4219e2c5510187b2490419afb14e6eda25862f01
Lucas Gonçalves Neves <art0066384@aluno.firjansenaisesi.com.br>
2026-02-27 18:10:58 -0300

    terceiro commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Header } from '../components/Header/Header.jsx'
+import { Sobre } from '../components/Sobre/Sobre.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
+    <Sobre categoria='sobreMim'/>
   </StrictMode>,
 )
